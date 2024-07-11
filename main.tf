ed1bd70e11c20cb1f850b7a9a420ad65efa7ac36
Andy Tran <54690164+aktran321@users.noreply.github.com>
2024-07-11 04:19:25 -0400

    VPC
    created the VPC and subents

---
+++ main.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Subnets
+resource "aws_subnet" "public_1" {
+  vpc_id            = aws_vpc.example.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id            = aws_vpc.example.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_3" {
+  vpc_id            = aws_vpc.example.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1c"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.example.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.example.id
+  cidr_block        = "10.0.5.0/24"
+  availability_zone = "us-east-1b"
+}
+
+resource "aws_subnet" "private_3" {
+  vpc_id            = aws_vpc.example.id
+  cidr_block        = "10.0.6.0/24"
+  availability_zone = "us-east-1c"
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
